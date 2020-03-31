043f3b211c9ecff2a522ad2101e22ea067c9da66
Cameron <cameroncheng@gmail.com>
2020-03-31 02:01:12 +0000

    Created assertEqual Function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('🔥🔥🔥' + 'Assertion Passed:' + ' ' + [actual] + ' ' + '===' + ' ' + [expected]);
+  } else {
+    console.log('❌❌❌' + 'Assertion Failed:' + ' ' + [actual] + ' ' + '!==' + ' ' + [expected]);
+  }
+
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 21);
